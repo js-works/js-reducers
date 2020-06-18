76fcf815072a4df4636a8717db1f38aae9fe3316
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2020-06-18 14:12:17 +0200

    Some updates

--- src/main/api/combineReducers.ts
+++ src/main/api/combineReducers.ts
@@ -1,26 +1,25 @@
 import Message from '../internal/types/Message'
 import Reducer from '../internal/types/Reducer'
 
-type Reducers = {
-  [key: string]: Reducer<any, any>
+type Reducers<M extends Message<any, any>> = {
+  [key: string]: Reducer<any, M>
 }
 
-type CombinedState<R extends Reducers> = {
+type CombinedState<R extends Reducers<any>> = {
   [K in keyof R]: R[K] extends Reducer<infer S, any> ? S : never
 }
 
-type MessageType<R extends Reducer<any, any>> = R extends Reducer<any, infer M>
-  ? M
-  : never
+type MessageType<R> = R extends Reducer<any, infer M> ? M : never
 
-type CombinedReducer<R extends Reducers> = Reducer<
-  CombinedState<R>,
-  { [K in keyof R]: MessageType<R[K]> }[keyof R]
->
+type CombinedReducer<
+  R extends Reducers<M>,
+  M extends Message<any, any>
+> = Reducer<CombinedState<R>, { [K in keyof R]: MessageType<R[K]> }[keyof R]>
 
-export default function combineReducers<R extends Reducers>(
-  reducers: R
-): CombinedReducer<R> {
+export default function combineReducers<
+  R extends Reducers<M>,
+  M extends Message<any, any>
+>(reducers: R): CombinedReducer<R, M> {
   const keys: (keyof R)[] = Object.keys(reducers)
 
   return (state: CombinedState<R> | undefined, msg: Message<any, any>) => {
